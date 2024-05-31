fe5762568716d04d25e5300413798b4ecdc601cd
Zach Barnes <zbarnz@yahoo.com>
2024-05-31 12:24:17 -0600

    Specified link type

--- src/resumeTypes.ts
+++ src/resumeTypes.ts
@@ -1,64 +1,66 @@
 export interface PersonalInfo {
-  name: string
-  email: string
-  phone?: string
-  address?: string
-  summary?: string
-  links?: {
-    name: string
-    url: string
-  }[]
+    name: string;
+    email: string;
+    phone?: string;
+    address?: string;
+    summary?: string;
+    links?: Link[];
+}
+
+export interface Link {
+    name: string;
+    url: string;
 }
 
 export interface WorkExperience {
-  company: string
-  position: string
-  startDate: string
-  endDate?: string
-  responsibilities: string[]
+    company: string;
+    position: string;
+    startDate: string;
+    endDate?: string;
+    responsibilities: string[];
 }
 
 export interface Education {
-  institution: string
-  degree: string
-  startDate?: string
-  endDate: string
+    institution: string;
+    degree: string;
+    startDate?: string;
+    endDate: string;
 }
 
 export interface Project {
-  title: string
-  description: string
-  responsibilities: string[]
-  website?: string
-  sourceCodeLink?: string
+    title: string;
+    description: string;
+    responsibilities: string[];
+    website?: string;
+    sourceCodeLink?: string;
 }
 
 export interface Certification {
-  name: string
-  dateAquired: string
+    name: string;
+    dateAquired: string;
 }
 
 export interface VolunteerWork {
-  name: string
-  startDate: string
-  description?: string
-  endDate?: string
+    name: string;
+    startDate: string;
+    description?: string;
+    endDate?: string;
 }
 
 export interface Language {
-  name: string
-  proficiency: string
+    name: string;
+    proficiency: string;
 }
 
 //Main Resume Data:
 
 export interface ResumeData {
-  personalInfo: PersonalInfo
-  workExperience?: WorkExperience[]
-  education?: Education[]
-  skills?: string[]
-  projects?: Project[]
-  certifications?: Certification[]
-  languages?: Language[]
-  volunteerWork?: VolunteerWork[]
+    personalInfo: PersonalInfo;
+    workExperience?: WorkExperience[];
+    education?: Education[];
+    skills?: string[];
+    projects?: Project[];
+    certifications?: Certification[];
+    languages?: Language[];
+    volunteerWork?: VolunteerWork[];
 }
